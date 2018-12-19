cb308667dfd97ea2fd3877b590ccdda3a81fd4b5
Rad Cirskis <nad2000@gmail.com>
2018-12-19 17:36:57 +0900

    own keep_if...

--- filter.cpp
+++ filter.cpp
@@ -19,9 +19,20 @@ vector<int> xs = {0, 1, 2, 3, 4};
 
 const auto ys = fplus::keep_if(is_even, xs);
 
+// own implementation of keep_if
+template <typename F, typename T> vector<T> keep_if0(F f, const vector<T>& xs) {
+	vector<T> ys;
+	for (auto v: xs) if (f(v)) ys.push_back(v);
+	return ys;
+}
+
 int main() {
 	vector<int> ys;
 	for (auto x : xs) if (IS_EVEN(x)) ys.push_back(x);
+	const auto ys2 = fplus::keep_if(is_even, xs);
+	const auto ys0 = keep_if0(is_even, xs);
 	PRINT_VEC(xs); cout << endl;
 	PRINT_VEC(ys); cout << endl;
+	PRINT_VEC(ys2); cout << endl;
+	PRINT_VEC(ys0); cout << endl;
 }
