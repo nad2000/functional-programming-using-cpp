113565436f1da8f04dbd502f532b81729d7f6497
Rad Cirskis <nad2000@gmail.com>
2018-12-20 15:15:48 +0900

    more generic solution with lambdas and templates

--- parse_and_product.cpp
+++ parse_and_product.cpp
@@ -17,14 +17,26 @@ int str_to_int(const string& str) {
 	return result;
 }
 
+template <typename Cont> auto sum(const Cont &values) {
+	return fplus::reduce(
+	     [](const auto a, const auto b) {return a+b;}, 1, values);
+}
+
+template <typename Cont> auto product(const Cont &values) {
+	return fplus::reduce(
+	     [](const auto a, const auto b) {return a*b;}, 1, values);
+}
+
 int main() {
 	const string input = "1,2,4,7,2,2,3";
 	auto parts = fplus::split(',', true, input);
 	const auto nums = fplus::transform(str_to_int, parts);
 	PRINT_VEC(parts); cout << endl;
 	PRINT_VEC(nums); cout << endl;
-	const auto result = fplus::reduce(multiplies<int>(), 1, nums);
-	cout << "RESULT: " << result << endl;
+	const auto sum_result = sum(nums);
+	const auto product_result = product(nums);
+	cout << "SUM: " << sum_result << endl;
+	cout << "PRODUCT: " << product_result << endl;
 }
 
 // TODO: modify so it works with floating point number too, change product to
