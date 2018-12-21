21100a140c5ed5f4365cb8ed1b81ffc651d4eca6
Rad Cirskis <nad2000@gmail.com>
2018-12-21 17:41:43 +0900

    the second ex...

--- currying_and_partial_functions.cpp
+++ currying_and_partial_functions.cpp
@@ -17,5 +17,15 @@ int main() {
 	const auto yss = fplus::transform(
 	  [](auto s) {return fplus::transform(square, s);}, xss);
 	cout << fplus::show(yss) <<endl;
-	
+
+	auto add_four = [](auto v1){
+		return [v1](auto v2) {
+			return [v1,v2](auto v3){
+				return [v1,v2,v3](auto v4){
+					return v1+v2+v3+v4;
+				};
+			};
+		};
+	};
+	cout << "ADD FOUR: " << add_four(1)(2)(3)(4) << endl;
 }
